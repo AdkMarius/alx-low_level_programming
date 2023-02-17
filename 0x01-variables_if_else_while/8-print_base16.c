8e1ae7104b08a151e4c322a48bdf580baefd5321
AdkMarius <adkmarius01@gmail.com>
2023-02-17 15:40:36 -0800

    caracter in base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+	char b;
+
+	for (a = 48; a <= 57; a++)
+		putchar(a);
+	for (b = 'a'; b <= 'f'; b++)
+		putchar(b);
+	putchar('\n');
+}
