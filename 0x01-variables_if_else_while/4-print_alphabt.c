81496453d6e26b19f65d46e587955b90e505742c
AdkMarius <adkmarius01@gmail.com>
2023-02-17 15:04:58 -0800

    alphabet except e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c == 'e' || c == 'q')
+			continue;
+		else
+			putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
