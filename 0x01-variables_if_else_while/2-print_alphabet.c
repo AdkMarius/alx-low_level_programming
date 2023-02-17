5a3bf7ed79ba0434067a8b2e4f436e406c66bc30
AdkMarius <adkmarius01@gmail.com>
2023-02-17 14:29:08 -0800

    print tolowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+/*
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	putchar(tolower(c));
+	return (0);
+}
