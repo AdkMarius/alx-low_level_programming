0fd5d925054cbdf93bfe65f53387e7484ddeaca3
AdkMarius <adkmarius01@gmail.com>
2023-02-21 12:09:56 -0800

    times tables

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,14 +9,12 @@ void times_table(void)
 {
 	int i, j;
 
-	for ( i = 0; i <= 9; i++)
+	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 9; j++)
 		{
-			if (j != 9 && (i * j) < 10)
-				printf("%d,  ", (i * j));
-			else if (j != 9 && (i * j) >= 10)
-				printf("%d, ", (i * j));
+			if (j != 9)
+				printf("%d,\t", (i * j));
 			else
 				printf("%d", (i * j));
 		}
