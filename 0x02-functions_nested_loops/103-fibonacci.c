f28da79b02460d0714750d3754ad6e4ca5bd450d
AdkMarius <adkmarius01@gmail.com>
2023-02-22 18:47:50 -0800

    sum of terms of fibonnacci suit

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - check the code.
+ *
+ * Return 0.
+ */
+int main(void)
+{
+	int i;
+	long int A[100], sum = 0;
+
+	A[0] = 1;
+	A[1] = 2;
+	for (i = 2; i < 100; i++)
+		A[i] = A[i - 1] + A[i - 2];
+	i = 0;
+	do
+	{
+		if(A[i] % 2 == 0)
+			sum = sum + A[i];
+		i++;
+	} while (A[i] < 4000000);
+	printf("%ld\n", sum);
+	return (0);
+}
