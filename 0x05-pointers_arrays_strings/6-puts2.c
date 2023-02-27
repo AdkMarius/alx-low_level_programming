e44dd61fbb5f4d95b97557d25097e6a337871372
AdkMarius <adkmarius01@gmail.com>
2023-02-27 14:21:53 -0800

    print other character

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,8 +11,10 @@ void puts2(char *str)
 {
 	int i;
 
-	for (i = 0; i < strlen(str); i += 2)
-		printf("%c", str[i]);
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i % 2 == 0)
+			printf("%c", str[i]);
+	}
 	printf("\n");
 }
-
