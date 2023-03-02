7a23f8f228cda27d18a071f0bde3657aaebd9104
AdkMarius <adkmarius01@gmail.com>
2023-03-02 11:33:16 -0800

    leet 1337

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,20 +8,21 @@
  */
 char *leet(char *s)
 {
-	int i;
+	char tab1[10] = {'a', 'e', 'o', 't', 'l', 'A', 'E', 'O', 'T', 'L'};
+	char tab2[10] = {'4', '3', '0', '7', '1', '4', '3', '0', '7', '1'};
+	int i, j;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		while (s[i] == 'a' || s[i] == 'A')
-			s[i] = '4';
-		while (s[i] == 'e' || s[i] == 'E')
-			s[i] = '3';
-		while (s[i] == 'o' || s[i] == 'O')
-			s[i] = '0';
-		while (s[i] == 't' || s[i] == 'T')
-			s[i] = '7';
-		while (s[i] == 'l' || s[i] == 'L')
-			s[i] = '1';
+		j = 0;
+		for (j = 0; j < 10; j++)
+		{
+			if (s[i] == tab1[j])
+			{
+				s[i] = tab2[j];
+				break;
+			}
+		}
 	}
 	return (s);
 }
