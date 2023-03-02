11e6f2801768f8030cc40cc9e2fd9680f027f641
AdkMarius <adkmarius01@gmail.com>
2023-03-01 16:36:31 -0800

    strncpy function

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strncpy: check the code
+ * @dest: A pointer to a string input
+ * @src: A pointer to a string input
+ * @n: An integer input
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	char *result;
+
+	result = strncpy(dest, src, n);
+	return (result);
+}
