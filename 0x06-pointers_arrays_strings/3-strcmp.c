8be46f0f87813896a3a302ada12107205ba41832
AdkMarius <adkmarius01@gmail.com>
2023-03-02 06:21:41 -0800

    strcmp function

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -2,9 +2,10 @@
 #include <string.h>
 
 /**
- * _strcmp: check the code
+ * _strcmp - check the code
  * @s1: A pointer to a string input
  * @s2: A pointer to a string input
+ *
  * Return: A positive or negative integer
  */
 int _strcmp(char *s1, char *s2)
