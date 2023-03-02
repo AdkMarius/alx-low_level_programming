6e90040e693ccd59a790f0ebab9a27267c8f615e
AdkMarius <adkmarius01@gmail.com>
2023-03-02 06:12:38 -0800

    strcat function

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,9 +2,10 @@
 #include <string.h>
 
 /**
- * _strcat: check the code
+ * _strcat - check the code
  * @dest: A pointer to a string input
  * @src: A pointer to a string input
+ *
  * Return: A pointer to a string
  */
 char *_strcat(char *dest, char *src)
