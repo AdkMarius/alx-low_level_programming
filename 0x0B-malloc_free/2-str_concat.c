ef5511948b8c92624f52f97f0f10a254243f0e9f
AdkMarius <adkmarius01@gmail.com>
2023-03-14 08:53:26 -0700

    Concatenate two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,32 +14,29 @@ char *str_concat(char *s1, char *s2)
 	char *ptr = NULL;
 	int i, j, size, len_s1, len_s2;
 
-	if (s1 == NULL && s2 != NULL)
-		ptr = s2;
-	else if (s1 != NULL && s2 == NULL)
-		ptr = s1;
-	else if (s1 == NULL && s2 == NULL)
-		ptr = NULL;
-	else
+	if (s1 == NULL && s2 == NULL)
+		return (NULL);
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	len_s1 = 0;
+	for (i = 0; s1[i] != '\0'; i++)
+		len_s1++;
+	len_s2 = 0;
+	for (i = 0; s2[i] != '\0'; i++)
+		len_s2++;
+	size = len_s1 + len_s2;
+	ptr = malloc(sizeof(char) * size + 1);
+	if (ptr == NULL)
+		return (NULL);
+	for (i = 0; i < len_s1; i++)
+		ptr[i] = s1[i];
+	for (j = 0; j < len_s2; j++)
 	{
-		len_s1 = 0;
-		for (i = 0; s1[i] != '\0'; i++)
-			len_s1++;
-		len_s2 = 0;
-		for (i = 0; s2[i] != '\0'; i++)
-			len_s2++;
-		size = len_s1 + len_s2;
-		ptr = malloc(sizeof(char) * size + 1);
-		if (ptr == NULL)
-			return (NULL);
-		for (i = 0; i < len_s1; i++)
-			ptr[i] = s1[i];
-		for (j = 0; j < len_s2; j++)
-		{
-			ptr[i] = s2[j];
-			i++;
-		}
-		ptr[i] = '\0';
+		ptr[i] = s2[j];
+		i++;
 	}
+	ptr[i] = '\0';
 	return (ptr);
 }
