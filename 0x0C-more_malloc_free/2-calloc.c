3fe87bc9ab5f0c51440eb0af23f0caaddfe9c454
AdkMarius <adkmarius01@gmail.com>
2023-03-16 15:27:27 -0700

    create calloc with malloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -13,12 +13,12 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	char *arr;
 	unsigned int i;
 
-	if (nmemb == 0 || size == 0)
+	if (nmemb <= 0 || size <= 0)
 		return (NULL);
 	arr = malloc(nmemb * size);
 	if (arr == NULL)
 		return (NULL);
 	for (i = 0; i < nmemb; i++)
-		arr[i] = '0';
+		arr[i] = 0;
 	return (arr);
 }
