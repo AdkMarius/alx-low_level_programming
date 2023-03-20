b616591b3bd1691a9ad80a16c844d952f39e1953
AdkMarius <adkmarius01@gmail.com>
2023-03-19 22:14:23 -0700

    working on macro in c programming

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print the name of current file
+ * @argc: number of arguments
+ * @argv: array2D of arguments
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	printf(__FILE__);
+	return (0);
+}
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,6 @@
+#ifndef HEADER_FILE
+#define HEDAER_FILE
+
+#define SUM(x, y) ((x) + (y))
+
+#endif
