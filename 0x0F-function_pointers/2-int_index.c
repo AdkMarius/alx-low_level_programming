857b5cdb6778b054bfe3d54c860c4e53aecbbff9
AdkMarius <adkmarius01@gmail.com>
2023-03-22 17:24:31 -0700

    search index of integer element

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,16 +11,13 @@
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
-	int OK;
 
-	if (size <= 0)
+	if (size <= 0 || array == NULL || cmp == NULL)
 		return (-1);
-	OK = 0;
 	for (i = 0; i < size; i++)
 	{
-		if (cmp(array[i]) > 0)
+		if (cmp(array[i]))
 		{
-			OK = 1;
 			return (i);
 			break;
 		}
