575159f9dc92d3a1298b0433f773e6d8c0a5c2ba
AdkMarius <adkmarius01@gmail.com>
2023-03-30 20:09:23 -0700

    delete list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,20 +1,34 @@
 #include "lists.h"
 
 /**
- * free_list - delete list
+ * free_list_head - delete list at head
  * @head: pointer on list
  *
  * Return: Nothing
  */
-void free_list(list_t *head)
+list_h *free_list_head(list_t *head)
 {
 	list_t *current;
 
-	while (head != NULL)
+	if (head != NULL)
 	{
 		current = head;
 		head = head->next;
 		free(current);
 	}
 	return (head);
+}
+
+/**
+ * free_list - delete all elements in the list
+ * @head: pointer on list
+ *
+ * Return: Nothing
+ */
+void free_list(list_t *head)
+{
+	if (head == NULL)
+		return ;
+	head = free_list_head(head);
+	free_list(head);
 }
