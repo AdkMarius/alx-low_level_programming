54f498a7c3527219740dcd410c3fe62ab0ad7776
AdkMarius <adkmarius01@gmail.com>
2023-03-30 21:14:25 -0700

    function which called before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * execute_before_main - function called before main function
+ *
+ * Return: Nothing
+ */
+void execute_before_main()
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
