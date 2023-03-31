16da7fa98bf2f893dc52f6a15d7dae098400d51e
AdkMarius <adkmarius01@gmail.com>
2023-03-30 17:33:29 -0700

    display single linked list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * print_list - function which display each element of a singly lnked list
+ * @h: pointer to a first node called head
+ *
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t count;
+	list_t *p;
+
+	count = 0;
+	p = h;
+	while (p != NULL)
+	{
+		if (p->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", p->len, p->str);
+		count++;
+		p = p->next;
+	}
+	return (count);
+}
